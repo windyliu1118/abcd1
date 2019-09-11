4580defd75542ea72b4738dc0595c8b905d39e27
i310213 <windy.liu@sap.com>
2019-09-11 21:33:33 +0800

    first

---
+++ BigNumber.java
@@ -0,0 +1,5 @@
+public class BigNumber {
+    public static void main(String[] args){
+
+    }
+}
---
+++ PrimaryTypes.java
@@ -0,0 +1,31 @@
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+
+public class PrimaryTypes {
+    public static void main(String[] args){
+        byte byteVar = 99;
+        System.out.println(byteVar);
+        short shortVar = -30000;
+        System.out.println(shortVar);
+
+        int intVar = 300000;
+        System.out.println(intVar);
+
+        long longVar = 99;
+
+        long bigLongVar = 9999999999L;
+        System.out.println(bigLongVar);
+
+        float floatVar = 100.1111111f;
+        System.out.println(floatVar);
+
+        double doubleVar = 100.1111111;
+        System.out.println(doubleVar);
+
+        boolean condition = true;
+        boolean fcoundition = false;
+        System.out.println(condition);
+
+        char ch = 'A';
+        System.out.println(ch);
+    }
+}
